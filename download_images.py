8eebbc0ece435878c2541fa3e94a1001ff23d193
Starches71 <155411307+Starches71@users.noreply.github.com>
2025-01-20 18:00:00 +0300

    Update download_images.py

--- download_images.py
+++ download_images.py
@@ -1,10 +1,11 @@
-
-from icrawler import ImageCrawler
 from icrawler.builtin import GoogleImageCrawler
 
 def download_images():
+    # Create a GoogleImageCrawler instance
     crawler = GoogleImageCrawler(storage={'root_dir': 'downloaded_images'})
-    crawler.crawl(keyword='San Francisco', max_num=10)  # You can adjust the keyword and number of images
+    
+    # Start crawling for images related to "San Francisco"
+    crawler.crawl(keyword='San Francisco', max_num=10)  # You can adjust the number of images
 
 if __name__ == "__main__":
     download_images()
