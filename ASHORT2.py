6de328a189cdc0343812e24967a06ad1bbab7c51
Starches71 <Starches71@users.noreply.github.com>
2025-06-14 09:30:01 +0000

    deploy: f4f4d9a21adf437911a4121f491b7a45cc08288f

--- ASHORT2.py
+++ ASHORT2.py
@@ -11,7 +11,7 @@ from google.genai import types
 def generate():
     # Load API key
     client = genai.Client(api_key=os.environ.get("GEMINI_API"))
-    model = "gemini-2.5-flash-preview-04-17"
+    model =  "gemini-2.0-flash"
 
     # Load YouTube link from file
     link_path = os.path.join("Vid", "yt_link")
